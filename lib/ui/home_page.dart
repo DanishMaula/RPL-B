0a55f905148ffde22a9bc404052a97959275f92a
ReihanFatilla <reihanfatilla@gmail.com>
2023-06-14 12:56:12 +0700

    feat: added home header

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:rpl_b/utils/style_manager.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+        child: Column(
+      children: [
+
+        // Header
+
+        Row(
+          children: [
+            Column(
+              children: [
+                Text(
+                  "Miqmeq",
+                  style: getBlackTextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                Text(
+                  "For RPL B Exhibition",
+                  style: getBlackTextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+            Container(
+              width: 45,
+              height: 45,
+              child: Image.asset(""),
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
+
+

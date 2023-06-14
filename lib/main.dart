6971950681627324b14f2c54d7163411f62a08ab
Miqdad <miqdaddeveloper@gmail.com>
2023-06-14 20:56:21 +0700

    (feat & fix) : Add see all page & add image_picker_util

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:rpl_b/provider/login_provider.dart';
 import 'package:rpl_b/ui/auth/login_page.dart';
 import 'package:rpl_b/ui/home_page.dart';
+import 'package:rpl_b/ui/see_all_page.dart';
 
 import 'firebase_options.dart';
 
@@ -24,10 +25,11 @@ class MyApp extends StatelessWidget {
       create: (context) => LoginProvider(),
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: LoginPage(),
+          initialRoute: SeeAllPage.routeName,
           routes: {
             LoginPage.routeName: (context) => const LoginPage(),
             HomePage.routeName: (context) => const HomePage(),
+            SeeAllPage.routeName: (context) => const SeeAllPage(),
           }),
     );
   }

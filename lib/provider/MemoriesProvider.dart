8d7baf05fa1497630acf3f84f448cd0cda3b42aa
Miqdad <miqdaddeveloper@gmail.com>
2023-06-16 08:30:42 +0700

    (feat & fix) : Add UploadMemoriesImage

--- lib/provider/MemoriesProvider.dart
+++ lib/provider/MemoriesProvider.dart
@@ -1,9 +1,21 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:rpl_b/data/model/memories.dart';
 import 'package:rpl_b/utils/helper.dart';
 
-class MemoriesProvider extends ChangeNotifier{
+import '../utils/result_state.dart';
+
+class MemoriesProvider extends ChangeNotifier {
+  String _message = '';
+
+  ResultState _state = ResultState.initialState;
+
+  ResultState get state => _state;
+
+  String get message => _message;
+
   Reference get firebaseStorage => FirebaseStorage.instance.ref();
 
   Future<List<Memories>> getHomeMemoriesList() async {
@@ -14,14 +26,33 @@ class MemoriesProvider extends ChangeNotifier{
     ListResult result = await parentRef.listAll();
 
     for (var item in result.items.take(10)) {
-      Memories memories = Memories(title: item.name.capitalize(), imageUrl: await item.getDownloadURL());
+      Memories memories = Memories(
+          title: item.name.capitalize(), imageUrl: await item.getDownloadURL());
       listMemories.add(memories);
       print(memories.imageUrl);
-
     }
 
     return listMemories;
   }
 
+  Future uploadMemoriesImage(File imageFile, String fileName) async {
+    try {
+      String imageUrl = '';
+      Reference storageRef = FirebaseStorage.instance.ref();
+      Reference dirImagesRef = storageRef.child('memories');
+      Reference fileRef = dirImagesRef.child(fileName);
 
+      await fileRef.putFile(File(imageFile.path));
+      imageUrl = await fileRef.getDownloadURL();
+      print(imageUrl);
+    } on FirebaseException catch (e) {
+      _state = ResultState.error;
+      _message = e.message!;
+      notifyListeners();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = e.toString();
+      notifyListeners();
+    }
+  }
 }

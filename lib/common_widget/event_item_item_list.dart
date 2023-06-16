9fae70032af275544533f1d828edcb928f4b84b0
Danish <danishmaulahasbi@gmail.com>
2023-06-16 08:52:34 +0700

    fix: fix error after merge

--- lib/common_widget/event_item_item_list.dart
+++ lib/common_widget/event_item_item_list.dart
@@ -19,47 +19,47 @@ class EventItemList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(
-          context,
-          EventDetailPage.routeName,
-          arguments: event.title,
-        );
-      },
-      child: Padding(
-        padding:
-            index != length - 1 ? EdgeInsets.only(right: 16) : EdgeInsets.zero,
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: SizedBox(
-                height: 160,
-                width: 120,
-                child: FadeInImage.assetNetwork(
-                  image: event.imageUrl,
-                  fit: BoxFit.cover,
-                  imageErrorBuilder: (a, b, c) {
-                    return Image.asset(
-                      "assets/images/gray_place_holder.png",
-                      fit: BoxFit.cover,
-                    );
-                  },
-                  placeholder: "assets/images/gray_place_holder.png",
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            EventDetailPage.routeName,
+            arguments: event.title,
+          );
+        },
+        child: Padding(
+          padding: index != length - 1
+              ? EdgeInsets.only(right: 16)
+              : EdgeInsets.zero,
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: SizedBox(
+                  height: 160,
+                  width: 120,
+                  child: FadeInImage.assetNetwork(
+                    image: event.imageUrl,
+                    fit: BoxFit.cover,
+                    imageErrorBuilder: (a, b, c) {
+                      return Image.asset(
+                        "assets/images/gray_place_holder.png",
+                        fit: BoxFit.cover,
+                      );
+                    },
+                    placeholder: "assets/images/gray_place_holder.png",
+                  ),
                 ),
               ),
-            ),
+              const SizedBox(
+                height: 16,
+                width: 10,
+              ),
+              Text(
+                event.title,
+                style: getBlackTextStyle(fontSize: 14),
+              )
+            ],
           ),
-          const SizedBox(
-            height: 16,
-            width: 10,
-          ),
-          Text(
-            event.title,
-            style: getBlackTextStyle(fontSize: 14),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
